b03107aa18e940b7142b68fa358850d7f8186783
yoldashsuhrab <101199946+yoldashsuhrab@users.noreply.github.com>
2022-05-05 11:33:43 +0600

    Add files via upload

---
+++ Assignment 2/Chapters/alpine.sh
@@ -0,0 +1,15 @@
+docker pull alpine
+
+docker images
+
+docker run alpine ls -l
+
+docker run alpine echo "hello from alpine"
+
+docker run alpine /bin/sh
+
+docker ps -a
+
+docker ps
+
+docker run -it alpine /bin/sh
